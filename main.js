090b40eac3d7d84a27c965312cbac90d970a20ab
david-dozer <dgmendoza1015@gmail.com>
2025-03-18 21:31:38 -0500

    embedded vids, updated bio

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ menuIcon.onclick = () => {
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-window.onScroll = () => {
+window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop -150;
@@ -49,52 +49,9 @@ ScrollReveal({
     delay: 200
 });
 
-// // Function to handle form submission
-// document.getElementById('news-form').addEventListener('submit', function(event) {
-//     event.preventDefault();
-
-//     // Get form values
-//     const title = document.getElementById('news-title').value;
-//     const content = document.getElementById('news-content').value;
-//     const date = document.getElementById('news-date').value;
-
-//     // Call function to add news post
-//     addNewsPost(title, content, date);
-
-//     // Clear form inputs
-//     document.getElementById('news-title').value = '';
-//     document.getElementById('news-content').value = '';
-//     document.getElementById('news-date').value = '';
-// });
-
-// // Function to add a new post to the news feed
-// function addNewsPost(title, content, date) {
-//     const newsFeed = document.getElementById('news-feed');
-
-//     // Create news post elements
-//     const newsPost = document.createElement('div');
-//     newsPost.classList.add('news-post');
-
-//     const titleElement = document.createElement('h3');
-//     titleElement.textContent = title;
-
-//     const contentElement = document.createElement('p');
-//     contentElement.textContent = content;
-
-//     const dateElement = document.createElement('p');
-//     dateElement.classList.add('date');
-//     dateElement.textContent = date;
-
-//     // Append elements to news post
-//     newsPost.appendChild(titleElement);
-//     newsPost.appendChild(contentElement);
-//     newsPost.appendChild(dateElement);
-
-//     // Add the new post to the news feed
-//     newsFeed.appendChild(newsPost);
-// }
-
 ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
 ScrollReveal().reveal('.home-img, .portfolio-container .portfolio-box', {origin: 'bottom'});
 ScrollReveal().reveal('.about-content h1, .about-img', {origin: 'left'});
 ScrollReveal().reveal('.home-content p, .download img, .about-content', {origin: 'right'});
+
+
